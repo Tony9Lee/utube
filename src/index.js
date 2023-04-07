564dbfff498a9e0712e7f1cb6e13297ee0705aa7
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-04-06 19:13:43 -0700

    UTube - Video Player and Sharing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App.js';
 import './index.css';
 import { Auth0Provider } from '@auth0/auth0-react';
 
